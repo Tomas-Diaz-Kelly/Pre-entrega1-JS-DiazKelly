c2824b67c56190af881c1b9517898a7b725866f1
tomas <tomasdiazkelly@outlook.com>
2023-02-22 12:39:28 -0300

    nuevaSubida

--- js/JS-1.js
+++ js/JS-1.js
@@ -1,28 +1,41 @@
-
-
-
-const zapatillas=1 
-const bermuda=2 
-const remera=3 
-const jean=4 
-const gorro=5 
-const medias=6 
-alert('hola elegi con el numero el producto que quieras 1-zapatillas 2-bermuda 3-remera 4-jean 5-gorro 6-medias')
-let numeroAIngresar = parseInt(prompt('ingrese el numero'))
-
-if (numeroAIngresar === 1){
-    alert('el valor de las zapatillas es de 1500')
-}else if(numeroAIngresar === 2){
-    alert('el valor de las bermuda es de 2000')
-}else if (numeroAIngresar === 3){
-    alert('el valor de las remera es de 2500')   
-}else if (numeroAIngresar === 4){
-    alert('el valor de las jean es de 3000')
-}else if (numeroAIngresar === 5){
-    alert('el valor de las gorro es de 950')
-}else if (numeroAIngresar === 6){
-    alert('el valor de las medias es de 380')
-}else {
-    alert('el numero ingresado esta mal')
+alert('ingrese nombre y apellido ')
+function datosUsuario(){
+    let nombre = prompt('ingrese nombre')
+    let apellido = prompt('ingrese apellido')
 }
 
+
+function seleccionDeProductos(){
+    let sumaDeProductos = 0;
+
+    let continuarCompra = true 
+    while(continuarCompra){
+        let productoSeleccionado = parseInt(prompt('Seleccione un producto: 1:velas , 2:difusores , 3:perfuminas'));
+        switch (productoSeleccionado){
+            case 1:
+                sumaDeProductos = sumaDeProductos + 1500
+                break;
+            case 2:
+                sumaDeProductos = sumaDeProductos + 1200
+                break;
+            case 3:
+                sumaDeProductos = sumaDeProductos + 1300
+                break;
+            default:
+                alert('el numero ingresado es erroneo')
+                break;
+        }
+        alert('El total de la compra es de:' + sumaDeProductos)
+        
+        let seguircomprando = prompt('Quiere continuar con la compra ? 1:si 2:no')
+        if (seguircomprando !== 1 ){
+            continuarCompra = false;
+        }else{
+            alert('gracias por tu compra')
+        }
+    }
+}
+
+
+datosUsuario()
+seleccionDeProductos()
