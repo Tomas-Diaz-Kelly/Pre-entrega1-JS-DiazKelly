c147f320b2f95be4c43099853cc4081f46476ca6
tomas <tomasdiazkelly@outlook.com>
2023-03-26 23:20:27 -0300

    modificaciones

--- js/JS-1.js
+++ js/JS-1.js
@@ -1,5 +1,6 @@
 // Tercer entrega
 
+
 const productos = [
     {id:1,nombre:'Difusores',precio:600},
     {id:2,nombre:'Velas',precio:800},
@@ -9,8 +10,7 @@ const productos = [
 
 const carrito = JSON.parse(localStorage.getItem('carrito')) || []
 const contenedorCarrito = document.getElementById("carrito")
-
-
+const contenedorTotal = document.getElementById("total");
 
 
 function listaDeProductos (){
@@ -38,33 +38,59 @@ function listaDeProductos (){
 function agregarAlCarrito(e){
     let id = Number(e.target.getAttribute('id'))
     let producto = productos.find((producto)=>producto.id === id)
-    carrito.push(producto)
-    renderizarCarrito()
-    localStorage.setItem("carrito", JSON.stringify(carrito))
+    let productoEnCarrito = carrito.find((producto)=>producto.id === id)
+  
+    if (productoEnCarrito) {
+      productoEnCarrito.cantidad += 1;
+      document.getElementById(`producto${productoEnCarrito.id}`).querySelector('h3:last-of-type').textContent = `Cantidad: ${productoEnCarrito.cantidad}`;
+    } else {
+      let productoConCantidad = {...producto, cantidad: 1};
+      carrito.push(productoConCantidad);
+  
+      let nuevaVariable = document.createElement('div');
+      nuevaVariable.innerHTML = `
+        <h3>${productoConCantidad.nombre}</h3>
+        <h3>$ ${productoConCantidad.precio}</h3>
+        <h3>Cantidad: ${productoConCantidad.cantidad}</h3>
+        <h3>Total: $ ${productoConCantidad.precio * productoConCantidad.cantidad}</h3>
+        `;
+        nuevaVariable.setAttribute('id',`producto${productoConCantidad.id}`);
+        contenedorCarrito.append(nuevaVariable);
+    }
+  
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
+
+
+
 
 const renderizarCarrito = () => {
-    contenedorCarrito.innerHTML = ""
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach((producto) => {
+        const nuevaVariable = document.createElement("div");
+        nuevaVariable.innerHTML = `
+        <h3>${producto.nombre}</h3>
+        <h3>$ ${producto.precio}</h3>
+        <h3>Cantidad: ${producto.cantidad}</h3>
+        <h3>Total: $ ${producto.precio * producto.cantidad}</h3>
+        `;
+        nuevaVariable.setAttribute("id", `producto${producto.id}`);
+        contenedorCarrito.append(nuevaVariable);
+    });
+    
+};
 
-    carrito.forEach(producto =>{
-        let productoEnCarrito = carrito.find((producto)=>producto.id === producto.id)
-        let indiceDelProducto = carrito.indexOf(productoEnCarrito)
-        
-        carrito[indiceDelProducto].cantidad+=1
-
-    const nuevaVariable = document.createElement('div') 
-    nuevaVariable.innerHTML =
-        `
-        <h3> ${producto.nombre} </h3>
-        <h3> ${producto.precio} </h3>       
-        <h3>${carrito[indiceDelProducto]}</h3>
-        `
-    contenedorCarrito.append(nuevaVariable)
-})
-}
-
+const calcularTotal = () => {
+    const total = carrito.reduce((acumulador, producto) => {
+        return acumulador + (producto.precio * producto.cantidad);
+    }, 0);
+    let contenedorTotal = document.createElement('div') 
+    contenedorTotal.innerHTML = `Total: $ ${total}`;
+    
+};
 
 
 listaDeProductos ()
 
 renderizarCarrito()
+calcularTotal();
